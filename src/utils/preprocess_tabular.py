b453f3c5202af2429838cb1d23d3240b877958aa
jmihan <mi.borovets@gmail.com>
2025-10-08 01:33:50 +0700

    Update preprocess_tabular.py

--- src/utils/preprocess_tabular.py
+++ src/utils/preprocess_tabular.py
@@ -24,21 +24,24 @@ OUTPUT_DATA_PATH = PROJECT_ROOT / 'data' / 'processed' / 'featured_dataset.csv'
 
 
 # --- Параметры обработки ---
-COLUMNS_TO_DROP = ['p', 'nTot', 'maintenance_flag']
+COLUMNS_TO_DROP = ['nTot', 'estimator_name', "E_mu_phys_est", "f_EC",]
 
 # Ключевые предикторы, для которых будем генерировать временные признаки.
 # Выбраны на основе EDA (высокая корреляция, "режимные" признаки).
 KEY_PREDICTORS = [
-    'E_mu_Z',          
+    'E_mu_Z',     
+    'E_nu1_Z',     
     'E_mu_Z_est',
-    'f_EC',
     'M_nu1_XX',
     'M_nu2_XX',
     'M_mu_XX',
     'polarizerVoltages[1]',
+    'polarizerVoltages[2]',
     'temp_1',
     'biasVoltage_1',
-    'estimator_name'    
+    'N_EC_rounds',
+    's',
+    'p',
 ]
 
 # --- Параметры генерации признаков ---
@@ -55,6 +58,58 @@ print("1. Загрузка сырых данных...")
 try:
     df = pd.read_csv(INPUT_DATA_PATH)
     print(f"  - Данные успешно загружены. Форма: {df.shape}")
+    df.columns = [
+        "block_id",
+        "frame_idx",
+        "E_mu_Z",
+        "E_mu_phys_est",
+        "E_mu_X",
+        "E_nu1_X",
+        "E_nu2_X",
+        "E_nu1_Z",
+        "E_nu2_Z",
+        "N_mu_X",
+        "M_mu_XX",
+        "M_mu_XZ",
+        "M_mu_X",
+        "N_mu_Z",
+        "M_mu_ZZ",
+        "M_mu_Z",
+        "N_nu1_X",
+        "M_nu1_XX",
+        "M_nu1_XZ",
+        "M_nu1_X",
+        "N_nu1_Z",
+        "M_nu1_ZZ",
+        "M_nu1_Z",
+        "N_nu2_X",
+        "M_nu2_XX",
+        "M_nu2_XZ",
+        "M_nu2_X",
+        "N_nu2_Z",
+        "M_nu2_ZZ",
+        "M_nu2_Z",
+        "nTot",
+        "bayesImVoltage",
+        "opticalPower",
+        "polarizerVoltages[0]",
+        "polarizerVoltages[1]",
+        "polarizerVoltages[2]",
+        "polarizerVoltages[3]",
+        "temp_1",
+        "biasVoltage_1",
+        "temp_2",
+        "biasVoltage_2",
+        "synErr",
+        "N_EC_rounds",
+        "maintenance_flag",
+        "estimator_name",
+        "f_EC",
+        "E_mu_Z_est",
+        "R",
+        "s",
+        "p",
+    ]
 except FileNotFoundError:
     print(f"Ошибка: Файл не найден по пути '{INPUT_DATA_PATH}'. Прерывание работы.")
     exit()
@@ -69,18 +124,12 @@ print("\n2. Предобработка и очистка данных...")
 df.drop(columns=COLUMNS_TO_DROP, inplace=True)
 print(f"  - Колонки {COLUMNS_TO_DROP} удалены.")
 
-# --- Обработка пропусков ---
-cols_to_fill = ['E_mu_phys_est', 'estimator_name']
-df[cols_to_fill] = df.groupby('block_id')[cols_to_fill].bfill().ffill()
-print(f"  - Пропуски в {cols_to_fill} обработаны методом ffill/bfill внутри каждой сессии.")
-
-# Проверка, что пропусков не осталось (кроме тех, что появятся после генерации лагов)
-initial_na_count = df.isnull().sum().sum()
-if initial_na_count == 0:
-    print("  - Все исходные пропуски успешно обработаны.")
-else:
-    print(f"  - Внимание: после обработки осталось {initial_na_count} пропусков.")
-
+df = df.rename(
+    columns={
+        "block_id": "id",
+        "frame_idx": "date",
+    }
+)
 
 # ==============================================================================
 # ГЕНЕРАЦИЯ ПРИЗНАКОВ (FEATURE ENGINEERING)
@@ -88,17 +137,20 @@ else:
 print("\n3. Генерация новых признаков...")
 tqdm.pandas(desc="Генерация признаков")
 
+# Сортируем данные на всякий случай, чтобы временные операции были корректны
+df.sort_values(by=['id', 'date'], inplace=True)
+
 # --- Временные признаки (Лаги) ---
 print("  - Создание лаговых признаков...")
 for col in tqdm(KEY_PREDICTORS, desc="  Лаги"):
     for lag in LAG_STEPS:
-        df[f'{col}_lag_{lag}'] = df.groupby('block_id')[col].shift(lag)
+        df[f'{col}_lag_{lag}'] = df.groupby('id')[col].shift(lag)
 
 # --- Временные признаки (Скользящие статистики) ---
 print("  - Создание скользящих статистик...")
 for col in tqdm(KEY_PREDICTORS, desc="  Скользящие стат."):
     for win in ROLLING_WINDOWS:
-        rolling_group = df.groupby('block_id')[col].rolling(window=win, min_periods=1)
+        rolling_group = df.groupby('id')[col].rolling(window=win, min_periods=1)
         
         df[f'{col}_roll_mean_{win}'] = rolling_group.mean().reset_index(level=0, drop=True)
         df[f'{col}_roll_std_{win}'] = rolling_group.std().reset_index(level=0, drop=True)
